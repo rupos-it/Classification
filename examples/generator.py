6264ec6e4499c29ac5708e800536631fcbc71cf4
Roberto Guanciale <guancio@guancio-studio.(none)>
2011-10-26 18:21:03 +0200

    Sempre refactoring

--- examples/generator.py
+++ examples/generator.py
@@ -2,33 +2,20 @@
 import random
 import time
 
-def formatAttr(key, value):
-    if type(value) == bool:
-        valueRepr = "true" if value else "false"
-        typeRepr = "BOOLEAN"
-    elif type(value) == float:
-        valueRepr = str(value)
-        typeRepr = "CONTINUOUS"
-    elif type(value) == int:
-        valueRepr = str(value)
-        typeRepr = "DISCRETE"
-    else:
-        valueRepr = str(value)
-        typeRepr = "LITERAL"
-    #TIMESTAMP: Attributes with timestamp type encode the UNIX timestamp (e.g., milliseconds since 01/01/1970) they represent, in the same form as discrete attributes.
-    #DURATION: Attributes with duration type encode their values in milliseconds, in the same form as discrete attributes.
-    return """
-     <attribute key="%s" value="%s" type="%s"/>
-    """ % (key, valueRepr, typeRepr)
 
-def defaultHook(startTime, attrs):
+
+def defaultActivityHook(startTime, attrs):
     t = startTime
     delta1 = random.randint(1, 30*60)
     delta2 = random.randint(delta1+1, 59*60)
     t0 = t+delta1
     t1 = t+delta2
-    return t0, t1, ""
+    return t0, t1
     
+def defaultChoiceHook(l, attr):
+    x = random.randint(0, len(l)-1)
+    return l[x]
+
 class Empty():
     def __init__(self):
         pass
@@ -40,11 +27,34 @@ class Entry:
         self.name = name
         self.hook = hook
         if self.hook is None:
-            self.hook = defaultHook
+            self.hook = defaultActivityHook
+
+    def formatAttr(self, key, value):
+        if type(value) == bool:
+            valueRepr = "true" if value else "false"
+            typeRepr = "BOOLEAN"
+        elif type(value) == float:
+            valueRepr = str(value)
+            typeRepr = "CONTINUOUS"
+        elif type(value) == int:
+            valueRepr = str(value)
+            typeRepr = "DISCRETE"
+        else:
+            valueRepr = str(value)
+            typeRepr = "LITERAL"
+        #TIMESTAMP: Attributes with timestamp type encode the UNIX timestamp (e.g., milliseconds since 01/01/1970) they represent, in the same form as discrete attributes.
+        #DURATION: Attributes with duration type encode their values in milliseconds, in the same form as discrete attributes.
+        return """
+     <attribute key="%s" value="%s" type="%s"/>
+    """ % (key, valueRepr, typeRepr)
+
     def gen(self, t, attrs):
-        t0, t1, data = self.hook(t, attrs)
+        t0, t1 = self.hook(t, attrs)
         tf0 = time.strftime("%Y-%m-%dT%H:%M:%S.000+01:00", time.gmtime(t0))
         tf1 = time.strftime("%Y-%m-%dT%H:%M:%S.000+01:00", time.gmtime(t1))
+        attr_str = ""
+        for attr in attrs.keys():
+            attr_str += self.formatAttr(attr, attrs[attr])
         res = """
         <event>
           %s
@@ -59,7 +69,7 @@ class Entry:
           <string key="concept:name" value="%s"/>
           <date key="time:timestamp" value="%s"/>
         </event>
-        """ % (data, self.name, tf0, self.name, tf1)
+        """ % (attr_str, self.name, tf0, self.name, tf1)
 
         return (res, t1)
 
@@ -78,10 +88,6 @@ class Recursion:
             res += res1
             it += 1
         return (res, t)
-
-def defaultChoiceHook(l, attr):
-    x = random.randint(0, len(l)-1)
-    return l[x]
     
 class Choice:
     def __init__(self, l, hook=None):
@@ -129,86 +135,8 @@ class Sequence:
         return (res, t)
         
 
-
-def requestHook(t, attrs):
-    dests = ["Italy", "UK", "USA", "Spain"]
-    dest = dests[random.randint(0, len(dests)-1)]
-    attrs["dest"] = dest
-
-    attrs["age"] = random.randint(18, 80)
-
-    t0, t1, data = defaultHook(t, attrs)
-    data = ""
-    for attr in attrs.keys():
-        data += """
-    <Attribute name="%s">%s</Attribute>
-    """%(attr, attrs[attr])
-    return t0,t1,data
-
-def carHook(t, attrs):
-    t0, t1, data = defaultHook(t, attrs)
-    if attrs["dest"] == "Italy":
-        t1 += 0
-    elif attrs["dest"] == "Spain" or attrs["dest"] == "USA":
-        t1 += random.randint(1, 60 * 60)
-    else:
-        t1 += random.randint(0.5 * 60 * 60, 3 * 60 * 60)
-    return t0,t1,data
-
-def airHook(t, attrs):
-    t0, t1, data = defaultHook(t, attrs)
-    if attrs["dest"] == "Italy":
-        t1 += 0
-    elif attrs["dest"] == "Spain" or attrs["dest"] == "UK":
-        t1 += random.randint(1, 60 * 60)
-    else:
-        t1 += random.randint(60 * 60, 5 * 60 * 60)
-    return t0,t1,data
-
-def driveHook(t, attrs):
-    t0, t1, data = defaultHook(t, attrs)
-    return t0,t1 + random.randint(1, 60 * 60),data
-
-def choiceHook(l, attrs):
-    if 30 < attrs["age"] < 60:
-        return l[1]
-    return l[0]
-
-def notifyHook(t, attrs):
-    # Urgente = ["Yes", "No"]
-    # Urgente = [True, False]
-    # Urgente = [0, 1]
-    Urgente = [0.0, 1.0]
-    urg = Urgente[random.randint(0, len(Urgente)-1)]
-    attrs["urg"] = urg
-
-
-    t0, t1, data = defaultHook(t, attrs)
-    data = ""
-    for attr in attrs.keys():
-        data += formatAttr(attr, attrs[attr])
-    return t0,t1,data
-
-def choiceHook(l, attrs):
-    if  attrs["urg"] == "Yes":
-        return l[1]
-    return l[0]
-
 	
-def gen_sequence(t):
-    acts = Sequence([ Entry("NotifyBug", notifyHook),
-                      Choice([
-                          Sequence([ Entry("CheckBug"),
-                                     Entry("FixBug" )
-                               ]),
-                          Entry("FixBug")
-                          ], choiceHook)
-                      ])
-                      
-    return acts.gen(t,{})[0]	     
-
-
-def gen_log(i):
+def generate_instance(process, i):
     t = time.time() + 3600 * i
     return """
 	<trace>
@@ -216,10 +144,11 @@ def gen_log(i):
 		<string key="description" value="PaperOne"/>
                 %s
         </trace>
-""" % (i, gen_sequence(t))
+""" % (i, process.gen(t,{})[0])
 
 
-print("""<?xml version="1.0" encoding="UTF-8" ?>
+def generate(process, instances):
+    print("""<?xml version="1.0" encoding="UTF-8" ?>
         <log xes.version="1.0" xes.features="nested-attributes" openxes.version="1.0RC7" xmlns="http://www.xes-standard.org/">
 	<extension name="Lifecycle" prefix="lifecycle" uri="http://www.xes-standard.org/lifecycle.xesext"/>
 	<extension name="Organizational" prefix="org" uri="http://www.xes-standard.org/org.xesext"/>
@@ -240,12 +169,12 @@ print("""<?xml version="1.0" encoding="UTF-8" ?>
 	<string key="concept:name" value="Paper"/>
 	<string key="lifecycle:model" value="standard"/>
 	<string key="description" value="Paper Submission"/>
-""")
+       """)
 
-for i in range(500):
-    print(gen_log(i))
-
-print("""
-</log>
-""")
+    for i in range(instances):
+        print(generate_instance(process, i))
+    
+    print("""
+        </log>
+          """)
 
