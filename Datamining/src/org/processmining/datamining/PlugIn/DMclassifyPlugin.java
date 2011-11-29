b329779dd1cb91488161b2715f0df4305c3dbd82
Hind <hind@hind-laptop.(none)>
2011-11-29 15:02:30 +0100

    aggiunto DMclassifyPlugin: prende le instances un classificatore addestrato e classifica l'input

---
+++ Datamining/src/org/processmining/datamining/PlugIn/DMclassifyPlugin.java
@@ -0,0 +1,34 @@
+package org.processmining.datamining.PlugIn;
+
+import org.processmining.contexts.uitopia.annotations.UITopiaVariant;
+import org.processmining.framework.plugin.PluginContext;
+import org.processmining.framework.plugin.annotations.Plugin;
+
+import weka.classifiers.Classifier;
+import weka.core.Instances;
+
+public class DMclassifyPlugin {
+	@Plugin(name = "Classify Istances to", 
+	        parameterLabels = { "Istances", "Classifier" }, 
+	        returnLabels = { "Weka Instance" }, 
+	        returnTypes = { Instances.class }  )
+	@UITopiaVariant(
+			uiLabel="Weka classify Instances",
+			affiliation = "University of Pisa",
+			author = "Hind",
+			email = "hind")
+			
+	public Instances classifyIstances(PluginContext context, Instances instances, Classifier classifier) throws Exception{
+		
+		double clsVal = 0;
+		
+		instances.setClassIndex(instances.numAttributes() - 1);
+		
+		for( int i=0; i<instances.numInstances(); i++){
+				clsVal = classifier.classifyInstance(instances.get(i));
+				instances.get(i).setClassValue(clsVal);
+		}
+		
+		return instances;
+	}
+}
